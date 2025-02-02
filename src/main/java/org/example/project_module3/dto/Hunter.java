18dc5566c37c53672e805f5421b5b6efa745ceea
wonderful9999 <ataichukuev3@gmail.com>
2025-02-02 20:02:35 +0500

    update

--- src/main/java/org/example/project_module3/dto/Hunter.java
+++ src/main/java/org/example/project_module3/dto/Hunter.java
@@ -1,8 +1,13 @@
 package org.example.project_module3.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@AllArgsConstructor
 public class Hunter {
     private final int id;
     private final String name;
